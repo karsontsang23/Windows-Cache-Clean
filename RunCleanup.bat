2fa953a5289ce0829a505f073e3f27ca2242095b
karsontsang23 <129140433+karsontsang23@users.noreply.github.com>
2026-03-08 14:09:24 -0400

    fix error

--- RunCleanup.bat
+++ RunCleanup.bat
@@ -10,5 +10,5 @@ for /f "usebackq tokens=1,* delims== eol=#" %%A in ("%~dp0.env") do (
     )
 )
 # 檢查可用 RAM <= %RAM_THRESHOLD_PERCENT% 才執行
-powershell -Command " $totalRAM = (Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory;$freeRAM = (Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory * 1KB;$freePercent = [math]::Round(($freeRAM / $totalRAM) * 100,2); if ($freePercent -le $env:RAM_THRESHOLD_PERCENT) {  Write-Host '可用 RAM' $freePercent ' <= ' $env:RAM_THRESHOLD_PERCENT '執行清理腳本' ; PowerShell.exe -Verb RunAs -NoProfile -ExecutionPolicy Bypass -File $env:Script_PATH\cleanup-win.ps1;}   else { Write-Host '可用 RAM '$freePercent ' >' $env:RAM_THRESHOLD_PERCENT  '不執行清理'; }"
+powershell -Command " $totalRAM = (Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory;$freeRAM = (Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory * 1KB;$freePercent = [math]::Round(($freeRAM / $totalRAM) * 100,2); if ($freePercent -le $env:RAM_THRESHOLD_PERCENT) {  Write-Host '可用 RAM' $freePercent ' <= ' $env:RAM_THRESHOLD_PERCENT '執行清理腳本' ;"Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%Script_PATH%\cleanup-win.ps1""'";}   else { Write-Host '可用 RAM '$freePercent ' >' $env:RAM_THRESHOLD_PERCENT  '不執行清理'; }"
 
